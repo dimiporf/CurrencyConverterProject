79f82fb871149e24cdf7e15fb8063ab8f624d4d5
Dimitrios Porfiropoulos <dimiporf@live.com>
2023-12-18 19:20:07 +0100

    Added Demo Values

--- CurrencyConverterProject/MainWindow.xaml.cs
+++ CurrencyConverterProject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace CurrencyConverterProject
         public MainWindow()
         {
             InitializeComponent();
+            BindCurrency();
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
@@ -33,6 +35,45 @@ namespace CurrencyConverterProject
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
+
+        }
+
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+
+            //Add display column in DataTable
+            dtCurrency.Columns.Add("Text");
+
+            //Add value column in DataTable
+            dtCurrency.Columns.Add("Value");
+
+            //Add rows in Datatable with text and value
+            dtCurrency.Rows.Add("--SELECT--", 0);
+            dtCurrency.Rows.Add("INR", 1);
+            dtCurrency.Rows.Add("USD", 75);
+            dtCurrency.Rows.Add("EUR", 85);
+            dtCurrency.Rows.Add("SAR", 20);
+            dtCurrency.Rows.Add("POUND", 5);
+            dtCurrency.Rows.Add("DEM", 43);
+
+            //The data to currency Combobox is assigned from datatable
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+
+            //DisplayMemberPath Property is used to display data in Combobox
+            cmbFromCurrency.DisplayMemberPath = "Text";
+
+            //SelectedValuePath property is used to set the value in Combobox
+            cmbFromCurrency.SelectedValuePath = "Value";
+
+            //SelectedIndex property is used to bind hint in the Combobox. The default value is Select.
+            cmbFromCurrency.SelectedIndex = 0;
+
+            //All properties are set for 'To Currency' Combobox as 'From Currency' Combobox
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
 
         }
     }
